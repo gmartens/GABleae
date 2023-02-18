64399613e5243a0cd7c5272f16a364271902fad3
GABE-CUSTOM-PC2\gabri <gabrielrobertmartens@gmail.com>
2023-02-18 17:47:44 -0600

    fix gitignore and add new engine location

---
+++ AleaeEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AleaeEngine
+{
+    class AleaeEngine
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
